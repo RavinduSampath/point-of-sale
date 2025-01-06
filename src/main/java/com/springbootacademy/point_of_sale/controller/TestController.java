09451ab0065668175104cbe2c60ef4d1126ead5e
Ravindu <praneethravindu50@gmail.com>
2025-01-06 18:56:54 +0530

    3rd one

--- src/main/java/com/springbootacademy/point_of_sale/controller/TestController.java
+++ src/main/java/com/springbootacademy/point_of_sale/controller/TestController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin
 @RequestMapping("api/v1/test")
 public class TestController {
+
     @GetMapping(path = "/get-text-1")
     public void getMyText(){
         String myText ="this is my first springboot app";
@@ -19,5 +20,12 @@ public class TestController {
     public void getMyText1(){
         String myText ="this is my second springboot app";
         System.out.println(myText);
+    }
+    @GetMapping("/get-text-3")
+    public String getMyText2(){
+        String myText ="this is my first springboot app";
+        System.out.println(myText);
+        return myText;
+
     }
 }
